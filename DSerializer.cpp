7f7ee00e1e58cff53f558bd8537ca644681bddf7
Dwyriel <dwyriel333@hotmail.com>
2023-08-06 19:03:28 -0300

    replace last magic character

--- DSerializer.cpp
+++ DSerializer.cpp
@@ -323,7 +323,7 @@ void DSerializer::DDocument::removeNewLinesTabsAndSpaces(std::string &string) {
 }
 
 void DSerializer::DDocument::readEntireString(std::string &string, std::string &outputString, size_t &index) {
-    throwParseErrorIf(string.find('"', index + 1) == std::string::npos);
+    throwParseErrorIf(string.find(QUOTATION_MARKS, index + 1) == std::string::npos);
     char currChar = 0, prevChar;
     while (true) {
         prevChar = currChar;
